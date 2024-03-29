308a86d75b4847d071f2613f4fb6a9b557826168
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-03-29 14:57:32 +0545

    storing place data in a ondevice sql database

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,12 +1,40 @@
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'dart:io';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final newPlace = Place(title: title, image: image, location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+
+    final filename = path.basename(image.path);
+
+    final copiedImage = await image.copy('${appDir.path}/$filename');
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
+
+    final dbPath = await sql.getDatabasesPath();
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+            "CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT , lat REAL, lng REAL , address TEXT)");
+      },
+      version: 1,
+    );
+    db.insert('user_places', {
+      'id ': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'lat': newPlace.location.longitude,
+      'lng': newPlace.location.longitude,
+      'address': newPlace.location.address,
+    });
     state = [newPlace, ...state];
   }
 }
